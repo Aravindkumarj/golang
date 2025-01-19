125384cc2d0ea4df156d218f2dc02d1bb1d0e701
Aravind Kumar Jagannathan <aravindkumarj@gmail.com>
2025-01-19 12:59:35 -0600

    01_19 stuff

---
+++ Iterations/iterator_test.go
@@ -0,0 +1,12 @@
+package iterations
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a")
+	want := "aaaaa"
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
---
+++ integers/adder.go
@@ -0,0 +1,13 @@
+// package integers
+package main
+
+import "fmt"
+
+// Add takes two integers and returns the sum of them.
+func Add(x, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(Add(3, 4))
+}
